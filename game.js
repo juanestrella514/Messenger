059bf5ed5d508895ad7a9c61ea16fbd6558be9da
Juan Estrella <jestrella@nyc.yearup.org>
2019-11-13 16:09:26 -0500

    updated lesson

---
+++ game.js
@@ -0,0 +1,27 @@
+// class Building {
+//   constructor(address) {
+//     this.address = address
+//     this.tenants = []
+//   }
+//
+//   addTenant(name) {
+//     this.tenants.push(name)
+//   }
+// }
+//
+
+// better way to write above
+// let building = new Building('123 Main st')
+//
+//
+// let tenants = []
+// let address = '123 street st'
+//
+// let t1 = 'bleep'
+// let t2 = 'bloop'
+//
+//
+// let building {
+//   tenants: [t1, t2],
+//   adress: address
+// }
--- main.js
+++ main.js
@@ -1,30 +1,71 @@
+const Conversations = []
+
+class Conversation {
+  constructor() {
+    this.id = Conversations.length + 1
+    this.messages = []
+    this.users = []
+  }
+  add(user) {
+    this.users.push(user)
+  }
+}
+
 class User {
   constructor(name){
     this.name = name
     this.inbox = []
+    this.blocedkUser = []
   }
 
-  sendMessages(receiver, content) {
-    let msg = new Message(receiver, content)
-    receiver.inbox.push(msg)
-    return `Your message to ${receiver.name} message has been sent`
+  sendMessages(convo, content) {
+    // if (convo.blocedkUsers.includes(this)) return 'Your message could not be sent'
+    let msg = new Message(content)
+    convo.inbox.push(msg)
   }
 
-// needs to show message read
+// needs to show message read and time seen
   readMessage(i) {
+    this.inbox[i].seen = true
+    this.inbox[i].seenAt = new Date()
     return this.inbox[i].content
   }
 
-  timeSeen(seen) {
-    return this.inbox(seen).content
+  viewUnread() {
+    let unread = []
+    for (let msg of this.inbox) {
+      if (msg.seen === false) {
+        unread.push(msg)
+        console.log(msg.content)
+      }
+    }
+    return unread
   }
-
+  viewRead() {
+    let read = []
+    for (let msg of this.inbox) {
+      if (msg.seen === true) {
+        read.push(msg)
+        console.log(msg.content)
+      }
+    }
+    return read
 }
 
+block(user) {
+  this.blockedUsers.push(user)
+
+  }
+}
+
+
+
 class Message {
-  constructor(receiver, content) {
-    this.receiver = receiver
+  constructor(convo, content) {
+    this.convo = convo
     this.content = content
+    this.seen = false
+    this.seenAt = null
   }
 }
 
